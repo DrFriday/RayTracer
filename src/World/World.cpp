9aa1ae3d14c2b26d9a0c9e4827e315bb0b13119d
Matt Bishop <matt.s.bishop@outlook.com>
2020-05-01 15:23:36 -0600

    Got scene from Figure 3.20 showing up.

--- src/World/World.cpp
+++ src/World/World.cpp
@@ -1,138 +1,132 @@
 #include "World.h"
 
+#include "../Events/Events.h"
+#include "../Tracers/MultipleObjects.h"
+#include "../Tracers/SingleSphere.h"
+#include "../UserInterface/RenderThread.h"
+#include "../Utilities/Constants.h"
+#include "../Utilities/Normal.h"
+#include "../Utilities/Point3D.h"
+#include "../Utilities/Ray.h"
+#include "../Utilities/Vector3D.h"
+#include "../GeometricObjects/Sphere.h"
+#include "../GeometricObjects/Plane.h"
+
+#include <iostream>
 #include <wx/wx.h>
 
-#include "../Utilities/Constants.h"
-#include "../Utilities/Vector3D.h"
-#include "../Utilities/Point3D.h"
-#include "../Utilities/Normal.h"
-#include "../Utilities/Ray.h"
-#include "../Tracers/SingleSphere.h"
-#include "../Tracers/MultipleObjects.h"
-
-#include "../UserInterface/RenderThread.h"
-
-#include "../Events/Events.h"
-#include <iostream>
-
-World::World()
-	: vp()
-	, background_color(black)
-	, sphere()
-	, tracer_prt(nullptr)
-{}
-
-void 
-World::build() 
+World::World() :
+    vp(), background_color(black), sphere(), tracer_prt(nullptr)
 {
-	// building ...
-	vp.set_hres(200);
-	vp.set_vres(200);
-	vp.set_pixel_size(1.0);
-	vp.set_gamma(1.0);
-
-	background_color = black;
-
-	//tracer_prt = new SingleSphere(this);
-	tracer_prt = new MultipleObjects(this);
-
-	auto sphere = new Sphere();
-	sphere->set_center(0.0);
-	sphere->set_radius(20.0);
-	sphere->set_color(white);
-
-	add_object(sphere);
-
-
-	auto sphere2 = new Sphere();
-	sphere2->set_center(Point3D(20, 0, 50));
-	sphere2->set_radius(20.0);
-	sphere2->set_color(red);
-
-	add_object(sphere2);
-
-
-	//sphere.set_center(0.0);
-	//sphere.set_radius(85.0);
 }
 
-void 
-World::render_scene() const 
+void World::build()
 {
-	RGBColor pixel_color;
-	Ray ray;
-	double zw = 100;
-	double x, y;
+    // building ...
+    vp.set_hres(200);
+    vp.set_vres(200);
+    vp.set_pixel_size(1.0);
+    vp.set_gamma(1.0);
 
-	open_window(vp.hres, vp.vres);
-	ray.d = Vector3D(0, 0, -1);
+    background_color = black;
 
-	for (int row = 0; row < vp.vres; row++)
-	{
-		for (int col = 0; col < vp.hres; col++)
-		{
-			x = vp.s * (col - 0.5 * (vp.hres - 1.0));
-			y = vp.s * (row - 0.5 * (vp.vres - 1.0));
-			ray.o = Point3D(x, y, zw);
+    //tracer_prt = new SingleSphere(this);
+    tracer_prt = new MultipleObjects(this);
 
-			pixel_color = tracer_prt->trace_ray(ray);
-			if (vp.gamma != 1.0)
-				pixel_color = pixel_color.powc(vp.inv_gamma);
+    auto yellowSphere = new Sphere();
+    yellowSphere->set_center(0.0);
+    yellowSphere->set_radius(60);
+    yellowSphere->set_color(RGBColor(1,1,0));
 
-			display_pixel(row, col, pixel_color);
-		}
-	}
+    add_object(yellowSphere);
+
+    auto redSphere = new Sphere();
+    redSphere->set_center(Point3D(0, -25, 0));
+    redSphere->set_radius(80);
+    redSphere->set_color(red);
+
+    add_object(redSphere);
+
+    //sphere.set_center(0.0);
+    //sphere.set_radius(85.0);
+
+	auto greenPlane = new Plane(Point3D(0, 0, 0), Normal(0, 1, 1));
+    greenPlane->set_color(RGBColor(0, 0.3, 0));
+    add_object(greenPlane);
 }
 
-void 
-World::open_window(const int hres, const int vres) const 
+void World::render_scene() const
 {
-	// opening ...
+    RGBColor pixel_color;
+    Ray ray;
+    double zw = 100;
+    double x, y;
+
+    open_window(vp.hres, vp.vres);
+    ray.d = Vector3D(0, 0, -1);
+
+    for (int row = 0; row < vp.vres; row++)
+    {
+        for (int col = 0; col < vp.hres; col++)
+        {
+            x = vp.s * (col - 0.5 * (vp.hres - 1.0));
+            y = vp.s * (row - 0.5 * (vp.vres - 1.0));
+            ray.o = Point3D(x, y, zw);
+
+            pixel_color = tracer_prt->trace_ray(ray);
+            if (vp.gamma != 1.0)
+                pixel_color = pixel_color.powc(vp.inv_gamma);
+
+            display_pixel(row, col, pixel_color);
+        }
+    }
 }
 
-void 
-World::display_pixel(const int row, const int column, const RGBColor& color) const
+void World::open_window(const int hres, const int vres) const
 {
-	int x = column;
-	int y = vp.vres - row - 1;
+    // opening ...
+}
 
-	/*if (vp.gamma != 1.0)
+void World::display_pixel(const int row, const int column, const RGBColor& color) const
+{
+    int x = column;
+    int y = vp.vres - row - 1;
+
+    /*if (vp.gamma != 1.0)
 		color = color.powc(vp.inv_gamma);*/
 
-	paintArea->setPixel(x, y, 
-		(int)(color.r * 255),
-		(int)(color.g * 255),
-		(int)(color.b * 255));
+    paintArea->setPixel(x, y,
+                        (int)(color.r * 255),
+                        (int)(color.g * 255),
+                        (int)(color.b * 255));
 }
 
-void
-World::setPaintArea(RenderThread* newPaintArea)
+void World::setPaintArea(RenderThread* newPaintArea)
 {
-	paintArea = newPaintArea;
+    paintArea = newPaintArea;
 }
 
-void
-World::add_object(GeometricObject* object_ptr)
+void World::add_object(GeometricObject* object_ptr)
 {
-	objects.push_back(object_ptr);
+    objects.push_back(object_ptr);
 }
 
 ShadeRec
 World::hit_bare_bones_objects(const Ray& ray) const
 {
-	ShadeRec sr(*this);
-	double t;
-	double tmin = kHugeValue;
+    ShadeRec sr(*this);
+    double t;
+    double tmin = kHugeValue;
 
-	for (auto object : objects)
-	{
-		if (object->hit(ray, t, sr) && t < tmin)
-		{
-			sr.hit_an_object = true;
-			sr.color = object->get_color(); 
-			tmin = t;
-		}
-	}
+    for (auto object : objects)
+    {
+        if (object->hit(ray, t, sr) && t < tmin)
+        {
+            sr.hit_an_object = true;
+            sr.color = object->get_color();
+            tmin = t;
+        }
+    }
 
-	return sr;
+    return sr;
 }
--- src/World/World.h
+++ src/World/World.h
@@ -1,49 +1,49 @@
 #pragma once
 
-#include <wx/wx.h>
-#include <vector>
-#include <memory>
-
-#include "ViewPlane.h"
-#include "../Utilities/RGBColor.h"
 #include "../GeometricObjects/Sphere.h"
 #include "../Tracers/Tracer.h"
+#include "../Utilities/RGBColor.h"
 #include "../Utilities/Ray.h"
+#include "ViewPlane.h"
+
+#include <memory>
+#include <vector>
+#include <wx/wx.h>
 
 class RenderThread;
 
-class World 
+class World
 {
-public:
-	ViewPlane vp;
-	RGBColor background_color;
-	Tracer* tracer_prt;
-	Sphere sphere;
-	std::vector<GeometricObject*> objects;
+  public:
+    ViewPlane vp;
+    RGBColor background_color;
+    Tracer* tracer_prt;
+    Sphere sphere;
+    std::vector<GeometricObject*> objects;
 
-	World();
-	
-	void 
-	build();
+    World();
 
-	void 
-	render_scene() const;
+    void
+    build();
 
-	void
-	open_window(const int hres, const int vres) const;
+    void
+    render_scene() const;
 
-	void
-	display_pixel(const int row, const int column, const RGBColor& color) const;
+    void
+    open_window(const int hres, const int vres) const;
 
-	void 
-	setPaintArea(RenderThread* paintArea);
+    void
+    display_pixel(const int row, const int column, const RGBColor& color) const;
 
-	void
-	add_object(GeometricObject* object_ptr);
+    void
+    setPaintArea(RenderThread* paintArea);
 
-	ShadeRec
-	hit_bare_bones_objects(const Ray& ray) const;
+    void
+    add_object(GeometricObject* object_ptr);
 
-private:
-	RenderThread* paintArea;
+    ShadeRec
+    hit_bare_bones_objects(const Ray& ray) const;
+
+  private:
+    RenderThread* paintArea;
 };
