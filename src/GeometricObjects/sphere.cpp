6e22d6ed41157522bdc3ef7eedaa90931cb403cc
Matt Bishop <matt.s.bishop@outlook.com>
2020-08-06 20:14:42 -0600

    Updating variable name and reason for conditions.

--- src/GeometricObjects/sphere.cpp
+++ src/GeometricObjects/sphere.cpp
@@ -7,15 +7,18 @@ bool Sphere::hit(const Ray& ray, double& tmin, ShadeRec& sr) const {
 	double a = ray.d * ray.d;
 	double b = 2.0 * temp * ray.d;
 	double c = temp * temp - radius * radius;
-	double disc = b * b - 4.0 * a * c;
+    double discriminant = b * b - 4.0 * a * c;
 
-	if (disc < 0.0)
+	// Complex roots
+    if (discriminant < 0.0)
 	{
 		return false;
 	}
+	// discriminant = 0.0, one real root, no plus and minus
+	// discriminant > 0.0, two real roots, has plus and minus
 	else
 	{
-		double e = sqrt(disc);
+        double e = sqrt(discriminant);
 		double denom = 2.0 * a;
 		t = (-b - e) / denom; // smaller root
 
